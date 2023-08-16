4f6f0177192e67724ad5bae335d53dd48acb8c69
anish-metaversity <anish@metaversitytechnologies.com>
2023-08-16 13:57:20 +0530

    env change

--- src/lucky7eucron/cron.module.ts
+++ src/lucky7eucron/cron.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
-// import { Lucky7eu, Lucky7euSchema } from 'model/t_lucky7eu';
 import { CasinoResult, CasinoResultSchema } from 'model/t_casino_result';
 import { Lucky7euService } from './cron.service';
 
--- src/teencron/cron.module.ts
+++ src/teencron/cron.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TeenService } from './cron.service';
 import { MongooseModule } from '@nestjs/mongoose';
-// import { Teen, TeenSchema } from 'model/t_teen';
 import { CasinoResult, CasinoResultSchema } from 'model/t_casino_result';
-
 @Module({
   imports: [
     // MongooseModule.forRoot('mongodb://localhost:27017'),
