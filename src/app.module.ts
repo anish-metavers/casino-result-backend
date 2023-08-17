3c0daeecb0565f92d8d4e85a6c61c4e8e1e79c4a
anish-metaversity <anish@metaversitytechnologies.com>
2023-08-17 11:24:40 +0530

    minor change number to string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TeenTwentyModule } from './teenTwentyCron/cron.module';
-import { Lucky7euModule } from './lucky7euCron/cron.module';
+import { Lucky7euModule } from './luckySevenCron/cron.module';
 import { amarAkabrAnthonyModule } from './amarAkbarAnthonyCron/cron.module';
 import { BollywoodTableModule } from './bollywoodTableCron/cron.module';
 import { TeenModule } from './teenCron/cron.module';
 import { DragonTigerModule } from './dragonTigerCron/cron.module';
-import { Poker20Module } from './poker20Cron/cron.module';
-import { PokerModule } from './poker1DayCron/cron.module';
-import { Poker6Module } from './poker6Cron/cron.module';
+import { Poker20Module } from './pokerTwentyCron/cron.module';
+import { PokerModule } from './pokerOneDayCron/cron.module';
+import { Poker6Module } from './pokerSixCron/cron.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
