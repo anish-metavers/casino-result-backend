866a140c5e89324eba4b4ef15a04a12fdaae9b24
anish-metaversity <anish@metaversitytechnologies.com>
2023-08-25 12:44:02 +0530

    dragon tiger table added

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { ConfigModule } from '@nestjs/config';
     BollywoodTableModule,
     LuckySevenModule,
     // TeenpattiModule,
-    // DragonTigerModule,
+    DragonTigerModule,
     // PokerTwentyModule,
     // PokerOneDayModule,
     // PokerSixModule,
